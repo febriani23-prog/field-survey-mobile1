3c8c3d9ef7aed7826597d8c94d1db36345c1b557
febriani23 <febrispra@gmail.com>
2026-07-28 09:27:09 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# flutter_application_febri
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/flutter_application_febri/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.flutter_application_febri
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
